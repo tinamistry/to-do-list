5bed22384c4b813caeee2184bb7e4797eed53755
Tina Mistry <tmistry10@gmail.com>
2024-01-08 21:01:39 -0500

    Adding basic ui for todo list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,39 @@
 
+import React from 'react';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SideBar from './components/SideBar'
+import Today from './components/Today'
+
+
+
+const theme = createTheme({
+  components:{
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          backgroundColor: '#efeff0', 
+          border: "none"
+        },
+      },
+    },
+  
+}});
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+
+    <div style={{ display: 'flex' }}>
+        <SideBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Today />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+  </ThemeProvider>
   );
 }
 
---
+++ client/src/components/SideBar.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { Typography } from '@mui/material';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function SideBar() {
+    const drawerWidth = 240;
+  return (
+    <div className="sidebar">
+       <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box',  },
+
+        }}
+      >
+        <Typography sx = {{ml: '10px', mt: '10px'}}>Tina's Lists</Typography>
+     <List sx = {{ ml : '-30px', mt: '50px'}}>
+           
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                  <WbSunnyIcon sx = {{color: '#fac905', mr: '4px'}}/>
+                  </ListItemIcon>
+                  <ListItemText primary={"today"} />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                  <CalendarTodayIcon sx = {{color: '#919191', mr: '4px'}}/>
+                  </ListItemIcon>
+                  <ListItemText primary={"future"} />
+                </ListItemButton>
+              </ListItem>
+     </List>
+
+     <List  sx = {{ ml : '-30px', mt: '50px'}}>
+         <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                  <AddIcon sx = {{color: '#919191', mr: '4px'}}/>
+                  </ListItemIcon>
+                  <ListItemText primary={"new list"} />
+                </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                  <DeleteIcon sx = {{color: '#919191', mr: '4px'}}/>
+                  </ListItemIcon>
+                  <ListItemText primary={"archive"} />
+                </ListItemButton>
+            </ListItem>
+        
+     </List>
+    
+      </Drawer>
+    </div>
+  );
+}
+
+export default SideBar;
