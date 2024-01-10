d6cb9ad73f351511609ed881899c042fc9ec14bb
Tina Mistry <tmistry10@gmail.com>
2024-01-10 13:53:20 -0500

    Added register page ui and routing to login page from register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,114 +1,86 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import '../styles/register.css'
 import NavBar from './NavBar'
-import { Typography } from '@mui/material';
-import logo from '../assets/Logo.png';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
-import Divider from '@mui/material/Divider';
-import Box from '@mui/material/Box'
+import logo from '../assets/Logo.png'
+import { Typography, Box, Grid, TextField, Button, Divider } from '@mui/material';
 
-function Register() {
+
+
+const RegisterForm = () => {
+    const navigate  = useNavigate();
+    const handleLoginButtonClick = () =>{
+       navigate('/')
+    }
+ 
   return (
     <div className="register">
-        <NavBar/>
-        <Typography variant = "h3">Register</Typography>
-        <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: '0.5rem',
-          marginTop: '5rem'
-        }}
-      >
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img src={logo} alt="Logo" width="50" height="50" />
-          <h1>Register</h1>
-        </div>
-        <Box component="form" noValidate>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                size="small"
-                autoComplete="fname"
-                name="firstName"
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                size="small"
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                size="small"
-                fullWidth
-                id="userName"
-                label="User Name"
-                name="userName"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                size="small"
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                size="small"
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                size="small"
-                fullWidth
-                name="passwordVerify"
-                label="Password Verify"
-                type="password"
-                id="passwordVerify"
-              />
-            </Grid>
-          </Grid>
-          <Button    fullWidth variant="contained" id="loginButton"  sx={{ mt: 3, mb: 2 }}>
-            Register
-          </Button>
-          <Grid container justifyContent="flex-end"></Grid>
-        </Box>
-        <Divider className="divider"> OR </Divider>
-        <Link className="link" to={'/login'}>
-          <Button  variant="contained" id="loginButton">
-            Login
-          </Button>
-        </Link>
-      </Box>
-    </Container>
-      
+       <div className = "header">
+           <NavBar/>
+       </div>
+       <div className = "register-form">
+            <div className = "form-heading">
+                <Typography variant = "h3">  <img src = {logo}  alt = "logo" style = {{width: '50px', height: '50px', marginBottom: '-7px'}}></img> Register</Typography>
+            </div>
+            <div className = 'form-inputs'>
+                <Box sx={{ flexGrow: 1,  display:'flex', marginTop: '0px', marginLeft: '150px'}}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                            <TextField 
+                            id="standard-basic" 
+                            label="First Name" 
+                            variant="standard" />
+                        </Grid>
+                        <Grid item xs={4}>
+                        <TextField 
+                            id="standard-basic" 
+                            label="Last Name" 
+                            variant="standard" />
+                        </Grid>
+                        <Grid item xs={8}>
+                        <TextField 
+                            fullWidth
+                            id="standard-basic" 
+                            label="Email" 
+                            variant="standard" />   
+                        </Grid>
+                        <Grid item xs={8}>
+                         <TextField 
+                            fullWidth
+                            id="standard-basic" 
+                            label="Password" 
+                            type = "password"
+                            variant="standard" />  
+                        </Grid>
+                        <Grid item xs={8}>
+                         <TextField 
+                            fullWidth
+                            id="standard-basic" 
+                            label="Confirm Password" 
+                            type = "password"
+                            variant="standard" />  
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Button fullWidth variant="contained">Register</Button>
+                        </Grid>
+                        <Grid item xs={8}>
+                          <Divider>OR</Divider>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Button onClick = {handleLoginButtonClick} fullWidth variant="contained">Login</Button>
+                        </Grid>
+                    </Grid>
+
+            </Box>
+          </div>
+   
+
+                
+       </div>
+
+        
     </div>
   );
-}
+};
 
-export default Register;
+export default RegisterForm;
